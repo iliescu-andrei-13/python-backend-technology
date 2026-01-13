b783164c4828180cec1da74f8550c8f4fd90df5d
Andrei <andrei.iliescu13102000@gmail.com>
2026-01-13 20:43:37 +0200

    Added models

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from viewer.models import Movie, Actors
 
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Actors)
--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=128, null=True, blank=True)
+    year = models.PositiveIntegerField(default=2012, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+
+# 1. Crearea unui model nou
+# 2. Adaugarea a doua elemente
+# 3. Actualizarea unui element
+# 4. Stergerea unui element
+
+class Actors(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    gender = models.CharField(max_length=20, null=True, blank=True)
